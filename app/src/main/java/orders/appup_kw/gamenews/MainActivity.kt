fd8b95d75759ae8feaec0e7b935a1eac754dc965
Danila-software-engineer <dendor0101@gmail.com>
2021-01-14 15:41:48 +0400

    Initial commit

---
+++ app/src/main/java/orders/appup_kw/gamenews/MainActivity.kt
@@ -0,0 +1,11 @@
+package orders.appup_kw.gamenews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
